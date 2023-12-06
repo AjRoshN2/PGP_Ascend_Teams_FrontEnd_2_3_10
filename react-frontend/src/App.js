be8d07330abeeea39a437b113c5b24cb233af8b7
Ajay Roshan N <a0r0gd5@homeoffice.wal-mart.com>
2023-12-06 20:46:31 +0530

    Latest with cart and wishlist

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
 import ProductComponent from './components/ProductComponent';
+import WishlistComponent from './components/WishlistComponent';
+import CartComponent from './components/CartComponent';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route  
+} from "react-router-dom";
 
 function App() {
   return (
-    <ProductComponent />
+    <>
+      <Routes>
+        <Route
+          path='/wishList'
+          element={<WishlistComponent />} />
+        <Route
+          path='/cart'
+          element={<CartComponent />} />
+      </Routes>
+
+      <ProductComponent />
+    </>
   );
 }
 
