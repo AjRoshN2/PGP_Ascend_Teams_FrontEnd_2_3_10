358ffcda449f88efc08b73fdc4370653e9155ccd
AjRoshN2 <ajayroshan.narayanasamy@gmail.com>
2023-12-06 20:17:39 +0530

    Update ProductsService.js - cart and wishlist

--- react-frontend/src/services/ProductsService.js
+++ react-frontend/src/services/ProductsService.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
-const ALL_CATEGORIES_API_BASE_URL = 'http://ascend-pgp-team2.eastus.cloudapp.azure.com:8765/products/categorylist';
-const PRODUCT_FOR_SELECTED_CATEGORY_API_BASE_URL = 'http://ascend-pgp-team2.eastus.cloudapp.azure.com:8765/products/category/';
-const All_PRODUCTS_API_BASE_URL = 'http://ascend-pgp-team2.eastus.cloudapp.azure.com:8765/products/allProducts';
-const PRODUCT_FOR_SEARCH_STRING_API_BASE_URL = 'http://ascend-pgp-team2.eastus.cloudapp.azure.com:8765/products/searchProductsByTitleNameOrShortDesc/';
+const ALL_CATEGORIES_API_BASE_URL = 'http://localhost:9010/products/categorylist';
+const PRODUCT_FOR_SELECTED_CATEGORY_API_BASE_URL = 'http://localhost:9010/products/category/';
+const All_PRODUCTS_API_BASE_URL = 'http://localhost:9010/products/allProducts';
+const PRODUCT_FOR_SEARCH_STRING_API_BASE_URL = 'http://localhost:9010/products/searchProductsByTitleNameOrShortDesc/';
+const ALL_WISHLIST_ITEMS_API_BASE_URL = 'http://localhost:9010/products/allItemsInWishlist';
+const ALL_CART_ITEMS_API_BASE_URL = 'http://localhost:9010/products/allItemsInCart';
+const ADD_ITEM_TO_CART_API_BASE_URL = 'http://localhost:9010/products/addItemToCart/';
+const ADD_ITEM_TO_WISHLIST_API_BASE_URL = 'http://localhost:9010/products/addItemToWishlist/';
 
 class ProductsService {
 
@@ -21,6 +25,22 @@ class ProductsService {
 
     getProductsForSearchString(searchString) {
         return axios.get(PRODUCT_FOR_SEARCH_STRING_API_BASE_URL + searchString);
+    }
+
+    getAllWishlistItems() {
+        return axios.get(ALL_WISHLIST_ITEMS_API_BASE_URL);
+    }
+
+    getAllCartItems() {
+        return axios.get(ALL_CART_ITEMS_API_BASE_URL);
+    }
+
+    addItemToCart(itemId) {
+        return axios.post(ADD_ITEM_TO_CART_API_BASE_URL + itemId);
+    }
+
+    addItemToWishlist(itemId) {
+        return axios.post(ADD_ITEM_TO_WISHLIST_API_BASE_URL + itemId);
     }
 }
 
